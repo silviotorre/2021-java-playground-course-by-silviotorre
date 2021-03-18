7be5da2e074dab154c306aceb479a1a5bab77dc5
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-03-18 17:55:35 +0100

    👋🌐

---
+++ sandbox/HelloWorld.java
@@ -0,0 +1,9 @@
+// This little,very litte joke print 👋🌐 "Hello world!"
+public class HelloWorld {
+    
+    private static void main() {
+        private String $👋= "Hello, ";
+        private String $🌐= "world!";
+        System.out.println($👋+$🌐);
+    }
+}
