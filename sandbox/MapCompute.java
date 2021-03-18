9e35a5a48b2a90b655d3826a6ae4437d8dafffff
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-03-18 10:12:56 +0100

    Added hashmap test

---
+++ sandbox/MapCompute.java
@@ -0,0 +1,39 @@
+import java.util.*; 
+
+public class MapCompute {
+    public static void main(String[] args) 
+    { 
+  
+        // Create a Map and add some values 
+        // very basic
+        /*
+        Map<String, Integer> map = new HashMap<>(); 
+        map.put("Key1", 12); 
+        map.put("Key2", 15); 
+        */
+
+        // HashMap godereccia
+        Map<String, Integer> map = new HashMap<>()
+                .withValue("Key3",23)
+                .withValue("Key4",33)
+                .withValue("Key5",43);
+
+        // print map details 
+        System.out.println("Map: " + map); 
+  
+        // remap the values 
+        // using compute method 
+        map.compute("Key1", (key, val) 
+                                -> (val == null) 
+                                       ? 1
+                                       : val + 1); 
+        map.compute("Key2", (key, val) 
+                                -> (val == null) 
+                                       ? 1
+                                       : val + 5); 
+  
+        // print new mapping 
+        System.out.println("New Map: " + map); 
+    } 
+} 
+
