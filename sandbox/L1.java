83eca216eacfaf563c83b1dd2393685ff740ac4f
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-03-18 06:59:04 +0100

    course example added

---
+++ sandbox/L1.java
@@ -0,0 +1,21 @@
+import java.util.logging.Level; 
+import java.util.logging.Logger; 
+  
+public class L1 { 
+  
+    public static void main(String[] args) 
+    { 
+  
+        // Create a Logger 
+        Logger logger 
+            = Logger.getLogger( 
+                L1.class.getName()); 
+  
+        // log messages using log(Level level, String msg) 
+        logger.log(Level.INFO, "This is message 1"); 
+        logger.log(Level.WARNING, "This is message 2"); 
+        logger.info("This also is info");
+        logger.finer("This is finer");
+    } 
+
+}
--- sandbox/S1.java
+++ sandbox/S1.java
@@ -2,8 +2,6 @@ import java.util.*;
 public class S1 {
 public static void main(String[] args) {
 List<String> strings = Arrays.asList("Lambda ", "expressions ", "test ", "2");
-for (String element : strings) {
-System.out.print(element);
-}
+strings.forEach((String s) -> System.out.print(s));
 }
 }
