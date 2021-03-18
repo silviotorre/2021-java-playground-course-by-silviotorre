559cde0eb095f7a38d12c58c5ddc21dcfb1aace9
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-03-18 06:34:51 +0100

    first commit

---
+++ esempi/strategy pattern/example/src/main/java/strategy/App.java
@@ -0,0 +1,22 @@
+package strategy;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    */
+public static void   main(String[] args) {
+    System.out.println("Hello World!");
+}
+
+}
