d5a726e93e7235f9b83cf63720ce5d52a9ceecaf
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-03-21 10:51:50 +0100

    minor fix

--- reverseString/ReverseString.java
+++ reverseString/ReverseString.java
@@ -1,21 +1,46 @@
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
+import java.util.concurrent.TimeUnit;
+
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
+
 
 public class ReverseString{
 
+    
      public static void main(String []args){
         //System.out.println(ReverseString.reverse("Hello World"));
        String inputString= "inputString";
- 
+       StringBuilder reverseString=new StringBuilder(inputString);
+       reverseString
+                    .reverse()
+                    .toString();
         
         CharacterIterator it = new StringCharacterIterator(inputString);
- 
-        while (it.current() != CharacterIterator.DONE) {
+        public void sleep(long timeout){
+            try {
+                while (it.current() != CharacterIterator.DONE) {
             
-            System.out.println(it.current());
-            it.next();
-           
+                    System.out.print(it.current());
+                    it.next();
+                    //TAKE A BREATH 
+                    TimeUnit
+                    .MILLISECONDS
+                    .sleep(timeout);
+                    
+                   
+                }
+                
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.print(it.current());
+                it.next();
+            }
         }
+ 
+        
+        System.out.println("\n"+reverseString
+            .toString());
       
         
     }
