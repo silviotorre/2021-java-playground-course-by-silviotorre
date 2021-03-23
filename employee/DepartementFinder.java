b90d7353b97940835bf159cb4f7912ad2d4b289b
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-03-23 20:19:16 +0100

    Adding departmentFinder

--- employee/DepartementFinder.java
+++ employee/DepartementFinder.java
@@ -14,21 +14,6 @@
  * limitations under the License.
  */
 
-public class Employee {
- 
-   String Name;
- 
-   
-   public String getName() {
-      return this.Name;
-   }
- 
-   public void setName(String value) {
-      this.Name = value;
-   }
- 
-   public static void main(String[] args) {
-       //some code
-   }
+public class DepartementFinder {
+    
 }
-
