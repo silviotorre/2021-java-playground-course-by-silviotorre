0fadbc89edb91aa2a5b8075b0a52251b8d7e8025
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-03-23 20:08:00 +0100

    Departement done

--- employee/Departement.java
+++ employee/Departement.java
@@ -1,3 +1,26 @@
+import java.util.List;
+
 public class Departement {
+    String departementName;
+    List<string>assignedEmployeesList;
+
+    public List<string> getAssignedEmployeesList() {
+        return this.assignedEmployeesList;
+    }
+
+    public void assignEmployee(string s) {
+        this.assignedEmployeesList.add(s);
+    }
+
+    public String getDepartementName() {
+        return this.departementName;
+    }
+
+    public void setDepartementName(String departementName) {
+        this.departementName = departementName;
+    }
+
+
+    
     
 }
