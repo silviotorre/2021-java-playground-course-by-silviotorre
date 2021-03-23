604c8a52f3e22a26953b2f4ea3fa5d61574bd87c
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-03-23 19:49:45 +0100

    working class ;-)

---
+++ employee/Employee.java
@@ -0,0 +1,20 @@
+
+
+public class Employee {
+ 
+   String Name;
+ 
+   
+   public String getName() {
+      return this.Name;
+   }
+ 
+   public void setName(String value) {
+      this.Name = value;
+   }
+ 
+   public static void main(String[] args) {
+       //some code
+   }
+}
+
