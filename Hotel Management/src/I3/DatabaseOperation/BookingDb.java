1a7da1a244dea258ef44be1717306626bf05f253
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-03-24 21:29:24 +0100

    working

--- Hotel Management/src/I3/DatabaseOperation/BookingDb.java
+++ Hotel Management/src/I3/DatabaseOperation/BookingDb.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package I3.DatabaseOperation;
 
 import I3.Classes.Booking;
@@ -49,17 +65,7 @@ public class BookingDb {
 
     }
 
-    public ResultSet getBookingInformation() {
-        try {
-            String query = "select * from booking";
-            statement = conn.prepareStatement(query);
-            result = statement.executeQuery();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex.toString() + "\n error coming from returning all booking DB Operation");
-        }
-
-        return result;
-    }
+   
 
     public ResultSet getABooking(int bookingId) {
         try {
